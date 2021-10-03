42ef71c60bf7949a01e881a629f2154d885e772a
Tony Hopkinson <tonyjhopkinson@protonmail.com>
2021-10-03 16:39:38 +0100

    RequestModel

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var requestModel = require('./lib/requestModel');
 const myConfig = require('./config/configuration.js').Configuration;
 http.createServer(function (req, res) {
+	//test
+	const rm = new requestModel.RequestModel(req);
+	console.log(rm.host);
+	//end of test
 	res.writeHead(200, {'Content-Type': 'text/json'});
 	res.end("{\"Response\":\"Nothing\"}");
 }).listen(myConfig.port);
