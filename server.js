90ad5e54e94aa559f9bd4383b3c08b55fbe754ef
Tony Hopkinson <tonyjhopkinson@protonmail.com>
2021-10-17 20:42:11 +0100

    Tidy

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 var http = require('http');
 const myConfig = require('./config/configuration.js').Configuration;
-const authenticate = require('./lib/authentication/authenticate');
-const router = require('./lib/routing/router');
-const RequestResponse = require('./lib/models/requestResponse');
-function process(req, res)
-{
-  var requestResponse = new RequestResponse(req);
-  requestResponse =  authenticate(requestResponse);
-  if (requestResponse.response.Ok())
-  {
-    requestResponse = router(requestResponse);
-  }
-  if (requestResponse.response.Ok())
-  {
-    requestResponse = requestResponse.response.action(requestResponse);
-  }
-  res.writeHead(requestResponse.response.status, {'Content-Type': requestResponse.response.type});
-  res.end(requestResponse.response.data);
-}
+const processor = require('./lib/processor');
 
 http.createServer(async function (req, res) 
 {
-	process(req,res);
+	processor(req,res);
 }).listen(myConfig.port);
